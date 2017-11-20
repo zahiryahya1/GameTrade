8dc4b02e503c717bf008ae071e1304bcbf32a7c8
zahir yahya <zahir_yahya@yahoo.com>
2017-11-20 14:33:41 -0800

    added mathjs dependencies

---
+++ public/javascripts/dashboard.js
@@ -0,0 +1,14 @@
+$(document).ready(function(){
+	console.log("on page load");
+	
+});
+
+
+function findMatch() {
+
+	// first make a call to find users in the max radius (50 miles) of user curr lat and lng,
+	// and return an array of users
+	// next compare users wants with the others have. create an array of games for each user obj = [ {user: , games: []]
+	// then compare other users wants with users have. (if user has at least 1 game the other wants, add to array)
+
+}
